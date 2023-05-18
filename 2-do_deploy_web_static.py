ebeb283df77def0cc3fef769ddee65c2df8f2a82
Njiraini23 <njirainii88@gmail.com>
2023-05-18 10:13:35 +0000

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" A python script that distributes the archive files on the web servers """
+"""
+A fabric script that distributes an archive on the web servers using do_deploy:
+"""
 from fabric.api import local, env, put, run
 import datetime
 import os
@@ -8,23 +10,25 @@ env.user = 'ubuntu'
 env.hosts = ['35.174.211.234', '100.25.141.83']
 
 
-def do_pack()
-"""
-source a .tgz archive in the web_static directory
-"""
-now = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-local('sudo mkdir -p ./versions')
-path = './versions/web_static_{}'.format(now)
-local('sudo tar -czvf {}.tgz web_static'.format(path))
-name = '{}.tgz'.format(path)
-if name:
-    return name
-else:
-    return None
+def do_pack():
+    """
+    The prototype to produce a .tgz file
+    """
+    now = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    local('sudo mkdir -p ./versions')
+    path = './versions/web_static_{}'.format(now)
+    local('sudo tar -czvf {}.tgz web_static'.format(path))
+    name = '{}.tgz'.format(path)
+    if name:
+        return name
+    else:
+        return None
 
 
 def do_deploy(archive_path):
-    """A script that distributes to the web servers"""
+    """
+    A script that distributes an archive to web servers
+    """
     if os.path.exists(archive_path):
         archive = archive_path.split('/')[-1]
         path = '/data/web_static/releases/{}/'.format(archive.split('.')[0])
@@ -36,7 +40,6 @@ def do_deploy(archive_path):
         run('sudo rm {}'.format(path_a))
         run('sudo mv {}/web_static/* {}'.format(path, path))
         run('sudo rm -rf {}/web_static'.format(path))
-        run('sudo rm -rf {}'.format(current))
         run('sudo rm -rf {}'.format(current))
         run('sudo ln -s {} {}'.format(path, current))
         print('New version deployed!')
