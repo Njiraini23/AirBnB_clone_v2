197ccfe1834bc414eb4cf1270a64629ee3f1497f
Njiraini23 <njirainii88@gmail.com>
2023-05-27 19:13:06 +0000

    changes

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -33,8 +33,8 @@ def python_text(text='is cool'):
     """Displays python followed by value of text variable
     replaces underscore with a space
     """
-    return "Python{}".format(text.replace('_' ' '))
+    return "Python {}".format(text.replace('_', ' '))
 
 
-if __name__ = "__main__"
-app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
