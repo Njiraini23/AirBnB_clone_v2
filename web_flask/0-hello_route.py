6c6936bdbe6298c1205422939009b37ec98b6fc6
Njiraini23 <njirainii88@gmail.com>
2023-05-27 15:03:27 +0000

    0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """
-A srcipt that starts Flask Web application
+a script that starts a flask web application
 """
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """Displays 'Hello HBNB!'"""
-    return 'Hello, HBNB!'
+def hello():
+    """Starts the application"""
+    return 'Hello HBNB!'
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
