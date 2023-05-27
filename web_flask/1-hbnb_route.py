5c60f841387eae68d562f99ca5c0ec103b4d2b73
Njiraini23 <njirainii88@gmail.com>
2023-05-27 15:08:11 +0000

    1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
 """
-a script that starts flask web application
+a script that starts a flask web application
 """
 from flask import Flask
-app =Flask (__name__)
+app = Flask(__name__)
 
 
-@app.route('/'strict_slashes=False)
-def hello_hbnb():
-    """ Displays 'Hello HBNB!'. """
+@app.route('/', strict_slashes=False)
+def hello():
+    """displays the hello hbnb web application"""
     return 'Hello HBNB!'
 
 
-app.route('/'hbnb, strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Displays 'HBNB'."""
-    return "HBNB"
+    """starts the hbnb route on web application"""
+    return 'HBNB'
 
 
-if __name__ = "__main__":
-    app.run(host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
