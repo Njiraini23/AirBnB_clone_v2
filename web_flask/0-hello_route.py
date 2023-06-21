ea536e56613bff2be4a023a7ad38a19c320f9ed4
Njiraini23 <njiraini88@gmail.com>
2023-06-21 09:25:45 +0000

    0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,7 +6,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/airbnb-onepage/', methods=['GET'])
 def hello():
     """Starts the application"""
     return 'Hello HBNB!'
